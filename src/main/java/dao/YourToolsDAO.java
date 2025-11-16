fbc31a3c2819204e2830bc12348b432662b7081e
Iag18 <iago.panho@gmail.com>
2025-11-16 19:10:37 -0300

    refactor(dao): altera visibilidade de listas estáticas para protected

--- src/main/java/dao/YourToolsDAO.java
+++ src/main/java/dao/YourToolsDAO.java
@@ -15,8 +15,8 @@ import java.util.logging.Logger;
 
 public class YourToolsDAO {
 
-    public static final ArrayList<Amigos> MinhaLista = new ArrayList<>();
-    public static final ArrayList<Ferramentas> MinhaListaFerramentas = new ArrayList<>();
+    protected static final ArrayList<Amigos> MinhaLista = new ArrayList<>();
+    protected static final ArrayList<Ferramentas> MinhaListaFerramentas = new ArrayList<>();
     Logger logger = Logger.getLogger(getClass().getName());
 
 
